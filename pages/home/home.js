8c67c9b7d625e65c168393616bf8bc059f763469
pengcheng <pengcheng@pengchengdeMacBook-Pro.local>
2018-04-08 23:18:00 +0800

    首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,5 +8,19 @@ Page({
             'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
             'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
         ]
+    },
+    _makePhoneCall: function () {
+        wx.makePhoneCall({
+            phoneNumber: '18510676740',
+            success: function (res) {
+                console.log('MakePhoneCall is Success!!', res)
+            },
+            fail: function (err) {
+                console.log('MakePhoneCall is Fail!!', err)
+            },
+            complete: function () { //不管成功还是失败都会调用次方法
+                console.log('MakePhoneCall is over!!')
+            }
+        })
     }
 })
